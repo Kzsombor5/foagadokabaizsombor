a7f77b22918118d03b2655f811565c03159456c8
Kzsombor5 <kabai.zsombi@gmail.com>
2025-10-08 14:50:01 +0200

    components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Roomlist from './components/Roomlist'
+import BookingForm from './components/BookingForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='App'>
+      <Header />      
+      <Roomlist />
+      <BookingForm />
+      <Footer />
 
     </div>
         
